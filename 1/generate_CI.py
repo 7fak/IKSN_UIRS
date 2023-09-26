cd22d4942496d3534a41d68c07a1a1b3f11ebb5c
Yury Barichev <ybarichev@yandex.ru>
2023-09-26 07:35:58 +0300

    Изменен Шрифт

--- 1/generate_CI.py
+++ 1/generate_CI.py
@@ -4,7 +4,7 @@ import random
 def text(symbol, output_path, size=100, RI_size=32):
     image = Image.new("RGB", (size, size), "white")
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype("./fonts/CoopBlack_Cyrillic_0.ttf", RI_size-5)
+    font = ImageFont.truetype("./fonts/appetite.ttf", RI_size-5)
     draw.text((random.randint(0, size-RI_size), random.randint(0, size-RI_size)), symbol, font=font, fill=(0, 0, 0))
     image.save(output_path)
 
